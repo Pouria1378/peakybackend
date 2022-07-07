9c862a54ef1ddeac6a7b77dcc801190c7c995f67
Pouria <82539162+Pouria1378@users.noreply.github.com>
2022-07-07 17:19:21 +0430

    s

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    // start point application
+    {
+      name: 'api',
+      script: 'app.js',
+      exec_mode: "cluster_mode",
+      instances: "2"
+    },
+
+    // cron jobs
+    {
+      "name": "cronJobs",
+      "script": "./cron.js",
+      "instances": 1
+    },
+
+    // workers
+    {
+      "name": "smsWorker",
+      "script": "./workers/smsWorker.js",
+      "instances": 1
+    }
+  ]
+};
